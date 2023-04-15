c72603443c82c1975c9af276b6eb78965ae2df92
balajijagtap <balajijagtap336@gmail.com>
2023-04-15 16:35:42 +0530

    add custom and server properties

--- src/main/java/com/balaji/demo/rest/FunRestController.java
+++ src/main/java/com/balaji/demo/rest/FunRestController.java
@@ -1,10 +1,23 @@
 package com.balaji.demo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String teamInfo(){
+        return "Coach: "+coachName+" Team: "+teamName;
+    }
+
     @GetMapping("/")
     public String sayHello(){
         return "Hello world";
